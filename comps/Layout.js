dce4f795e75b3cc56328c35ff7e7d77a6ef3def4
gsabury <gh.sabury@gmail.com>
2023-10-03 11:38:40 +0430

    all code moved

---
+++ comps/Layout.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navabar";
+
+const Layout = ({ children }) => {
+    return (
+        <div className="content">
+            <Navbar />
+            {children}
+            <Footer />
+        </div>
+    );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <Link href="/ninjas/" className={styles.btn}>
+          See Ninja Listing
+        </Link>
+      </div>
+    </>
+  )
+}
